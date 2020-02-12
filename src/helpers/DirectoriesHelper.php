3f875d106f03225f947953b9bf40f945bcbd6b56
Kapsid <urbanczyk@master.cz>
2020-02-12 19:15:58 +0100

    Small fix for recursive removing dirs

--- src/helpers/DirectoriesHelper.php
+++ src/helpers/DirectoriesHelper.php
@@ -2,10 +2,14 @@
 
 namespace DirSync\helpers;
 
+use DirSync\DirSync;
+use Tracy\Debugger;
+
 /**
  * Class DirectoriesHelper
  * @package DirSync\helpers
  * This is a helper class for working with directories
+ * @author Martin Urbanczyk
  */
 class DirectoriesHelper
 {
@@ -16,16 +20,23 @@ class DirectoriesHelper
      * @param $actualDirectory
      * @param bool $subfolder
      */
-    private static function removeDirectory($listOfDirectories, $actualDirectory, $subfolder = false): void{
+    private static function removeDirectory($listOfDirectories, $actualDirectory = false, $subfolder = false): void{
         if(!empty($listOfDirectories)){
             foreach($listOfDirectories as $unnecessaryDir){
-                $fullPathDirectory = $actualDirectory.'/'.$unnecessaryDir;
+                if($actualDirectory === true){
+                    $fullPathDirectory = $unnecessaryDir;
+                }
+                else{
+                    $fullPathDirectory = $actualDirectory.'/'.$unnecessaryDir;
+                }
+
+                Debugger::barDump($fullPathDirectory,"full path");
 
                 if( is_dir($fullPathDirectory) !== false )
                 {
                     if (PHP_OS === 'Windows')
                     {
-                        //exec(sprintf("rd /s /q %s/", escapeshellarg($actualDirectory.$actualArrays[$directoryName].$subRemoveDir)));
+                        //exec(sprintf("rd /s /q %s/", escapeshellarg($actualDirectory.$actualJsonArray[$directoryName].$subRemoveDir)));
                     }
                     else
                     {
@@ -55,47 +66,59 @@ class DirectoriesHelper
         return $basePathArray;
     }
 
-
     /**
      * Recursive function for creating directories structure
+     * This function should be much simples (e.g. the whole process of going through json dirs should be in another function)
      * @param $actualDirectory
-     * @param $actualArrays
+     * @param $actualJsonArray Json passed in DirSync init
      */
-    public static function createDirectories($actualDirectory, $actualArrays) : void {
+    public static function syncDirectories($actualDirectory, $actualJsonArray, $options = false) : void {
+
+        // Adding slash to actual dir
         $actualDirectory .= '/';
 
         // Load folders in actual dir
         $currentDirs = self::getBasepathDirectoriesArray(array_filter(glob($actualDirectory.'*'), 'is_dir'));
+
+        // Sorting for later comparison
         sort($currentDirs);
-        $jsonDirs = array_keys($actualArrays);
+
+        $jsonDirs = array_keys(ActionsHelper::parseActions($actualJsonArray));
+
+
+        // Sorting for later comparison
         sort($jsonDirs);
 
+        // Going through all filtered directories in json
         foreach($jsonDirs as $directoryName){
 
+            // If this dir is already created remove it from currents dirs
             if (($key = array_search($directoryName, $currentDirs)) !== false) {
                 unset($currentDirs[$key]);
             }
 
             if( is_dir($actualDirectory.$directoryName) === false )
             {
+                // There should be a check if there are not any banned characters that can be in the folder
                 mkdir($actualDirectory.$directoryName);
             }
 
 
-            if(isset($actualArrays[$directoryName])){
-                if(is_array($actualArrays[$directoryName])){
-                    self::createDirectories($actualDirectory.$directoryName, $actualArrays[$directoryName]);
+            if(isset($actualJsonArray[$directoryName])){
+                if(is_array($actualJsonArray[$directoryName])){
+                    self::syncDirectories($actualDirectory.$directoryName, $actualJsonArray[$directoryName]);
                 }
             }
 
             else{
-                $subRemoveDirs = array_filter(glob($actualDirectory.$actualArrays[$directoryName].$directoryName.'/*'), 'is_dir');
-                self::removeDirectory($subRemoveDirs,$actualDirectory.$actualArrays[$directoryName].$directoryName);
+                $subRemoveDirs = array_filter(glob($actualDirectory.$actualJsonArray[$directoryName].$directoryName.'/*'), 'is_dir');
+                self::removeDirectory($subRemoveDirs);
             }
 
         }
 
-        // Removing unnecesarry dirs
+
+        // Removing unnecesarry dirs in this level
         self::removeDirectory($currentDirs,$actualDirectory);
     }
 
